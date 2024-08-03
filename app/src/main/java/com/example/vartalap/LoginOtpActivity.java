e6f5c34277f8ec83391208d1e0834762665a1828
AmanRawat <134505465+RawatAmann@users.noreply.github.com>
2024-08-03 22:05:47 +0530

    update

---
+++ app/src/main/java/com/example/vartalap/LoginOtpActivity.java
@@ -0,0 +1,16 @@
+package com.example.vartalap;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginOtpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_otp);
+
+    }
+}
